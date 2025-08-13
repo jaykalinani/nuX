51f7158b3ff213842052cd71530de9847293f4f7
Allen Wen <acw6923@g.rit.edu>
2025-08-13 16:01:36 -0500

    nuX_FakeRates: fix init bug

--- nuX_FakeRates/src/init_fakerates.cxx
+++ nuX_FakeRates/src/init_fakerates.cxx
@@ -5,6 +5,7 @@
 #include "nuX_fakerates.hxx"
 
 namespace nuX_FakeRates {
+using namespace amrex;
 
 FakeRatesDef* global_fakerates = nullptr;
 
@@ -12,6 +13,9 @@ extern "C" void nuX_FakeRates_Setup(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_nuX_FakeRates_Setup;
   DECLARE_CCTK_PARAMETERS;
 
+  global_fakerates = (FakeRatesDef *)The_Managed_Arena()->alloc(
+      sizeof *global_fakerates);
+  new (global_fakerates) FakeRatesDef;
   global_fakerates->init();
 
 }
