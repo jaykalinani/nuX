a12f6653832605fd8859ccd5360fe13e4ccae902
Allen Wen <acw6923@g.rit.edu>
2026-02-11 16:17:43 -0600

    nuX_M1: works with poisoning

--- nuX_M1/src/nuX_M1_init_fluxes_rhs.cxx
+++ nuX_M1/src/nuX_M1_init_fluxes_rhs.cxx
@@ -28,6 +28,13 @@ extern "C" void nuX_M1_InitFluxesRHS(CCTK_ARGUMENTS) {
           rFx_rhs[i4D] = 0.0;
           rFy_rhs[i4D] = 0.0;
           rFz_rhs[i4D] = 0.0;
+
+          abs_0[i4D] = 0.0;
+          abs_1[i4D] = 0.0;
+          eta_0[i4D] = 0.0;
+          eta_1[i4D] = 0.0;
+          scat_1[i4D] = 0.0;
+          nueave[i4D] = 0.0;
         }
       });
 
