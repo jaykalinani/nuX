c3a1826f199fb5db646b2ecd781026d0e7ded9f7
jaykalinani <jaykalinani@gmail.com>
2026-02-11 03:06:51 -0600

    nuX_RatesToy: remove volform usage, clang-format

--- nuX_RatesToy/src/init_ratestoy.cxx
+++ nuX_RatesToy/src/init_ratestoy.cxx
@@ -12,24 +12,23 @@ extern "C" void nuX_RatesToy_Init(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS
 
   const GridDescBaseDevice grid(cctkGH);
-  const GF3D2layout layout2(cctkGH, {1, 1, 1});
+  const GF3D2layout layout_cc(cctkGH, {1, 1, 1});
 
   grid.loop_all_device<1, 1, 1>(
       grid.nghostzones,
       [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        for (int ig = 0; ig < ngroups * nspecies; ++ig) {
+          int const i4D = layout_cc.linear(p.i, p.j, p.k, ig);
 
-			for (int ig = 0; ig < ngroups * nspecies; ++ig) {
-				int const i4D = layout2.linear(p.i, p.j, p.k, ig);
-
-				rnt[i4D] = set_n;
-				rJt[i4D] = set_J;
-				rHt_t[i4D] = set_Ht;
-				rHxt[i4D] = set_Hx;
-				rHyt[i4D] = set_Hy;
-				rHzt[i4D] = set_Hz;
-				chit[i4D] = set_chi;
-			}
-		});
+          rnt[i4D] = set_n;
+          rJt[i4D] = set_J;
+          rHt_t[i4D] = set_Ht;
+          rHxt[i4D] = set_Hx;
+          rHyt[i4D] = set_Hy;
+          rHzt[i4D] = set_Hz;
+          chit[i4D] = set_chi;
+        }
+      });
 }
 
-} // namespace
+} // namespace nuX_RatesToy
