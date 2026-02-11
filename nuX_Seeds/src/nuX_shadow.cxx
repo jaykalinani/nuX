f3c3898f5547d2ee1e91f8369b8af350152f631f
jaykalinani <jaykalinani@gmail.com>
2026-02-11 06:50:39 -0600

    nuX_Seeds: update shadow test

--- nuX_Seeds/src/nuX_shadow.cxx
+++ nuX_Seeds/src/nuX_shadow.cxx
@@ -35,6 +35,9 @@ extern "C" void nuX_Seeds_SetupHydroTest_shadow(CCTK_ARGUMENTS) {
 
   const GridDescBaseDevice grid(cctkGH);
   const GF3D2layout layout_cc(cctkGH, {1, 1, 1});
+  const GF3D2layout layout3(cctkGH, {1, 0, 0});
+  const GF3D2layout layout4(cctkGH, {0, 1, 0});
+  const GF3D2layout layout5(cctkGH, {0, 0, 1});
 
   grid.loop_all_device<1, 1, 1>(
       grid.nghostzones, [=] CCTK_DEVICE(const PointDesc &p) {
@@ -49,6 +52,27 @@ extern "C" void nuX_Seeds_SetupHydroTest_shadow(CCTK_ARGUMENTS) {
 
         press[ijk] =
             eos_3p_ig->press_from_valid_rho_eps_ye(rho[ijk], eps[ijk], Ye[ijk]);
+      });
+
+  grid.loop_all_device<1, 0, 0>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const int ijk = layout3.linear(p.i, p.j, p.k);
+        Avec_x[ijk] = 0.;
+      });
+
+  grid.loop_all_device<0, 1, 0>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const int ijk = layout4.linear(p.i, p.j, p.k);
+        Avec_y[ijk] = 0.;
+      });
+
+  grid.loop_all_device<0, 0, 1>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const int ijk = layout5.linear(p.i, p.j, p.k);
+        Avec_z[ijk] = 0.;
       });
 }
 
