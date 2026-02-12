d822daf1c330483a4555fe99824fbf6396398b76
Allen Wen <acw6923@rit.edu>
2026-02-12 14:47:29 -0600

    nuX_Seeds: init Avec for sphere test

--- nuX_Seeds/src/nuX_sphere.cxx
+++ nuX_Seeds/src/nuX_sphere.cxx
@@ -38,6 +38,9 @@ extern "C" void nuX_Seeds_SetupHydroTest_sphere(CCTK_ARGUMENTS) {
 
   const GridDescBaseDevice grid(cctkGH);
   const GF3D2layout layout_cc(cctkGH, {1, 1, 1});
+  const GF3D2layout layout3(cctkGH, {1, 0, 0});
+  const GF3D2layout layout4(cctkGH, {0, 1, 0});
+  const GF3D2layout layout5(cctkGH, {0, 0, 1});
   grid.loop_all_device<1, 1, 1>(grid.nghostzones, [=] CCTK_DEVICE(
                                                       const PointDesc &p) {
     const int ijk = layout_cc.linear(p.i, p.j, p.k);
@@ -55,6 +58,27 @@ extern "C" void nuX_Seeds_SetupHydroTest_sphere(CCTK_ARGUMENTS) {
           eos_3p_ig->press_from_valid_rho_eps_ye(rho[ijk], eps[ijk], Ye[ijk]);
     }
   });
+
+  grid.loop_all_device<1, 0, 0>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const int ijk = layout3.linear(p.i, p.j, p.k);
+        Avec_x[ijk] = 0.;
+      });
+
+  grid.loop_all_device<0, 1, 0>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const int ijk = layout4.linear(p.i, p.j, p.k);
+        Avec_y[ijk] = 0.;
+      });
+
+  grid.loop_all_device<0, 0, 1>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const int ijk = layout5.linear(p.i, p.j, p.k);
+        Avec_z[ijk] = 0.;
+      });
 }
 
 extern "C" void nuX_Seeds_SetupNeutTest_sphere(CCTK_ARGUMENTS) {
